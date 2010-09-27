cecf551767b565a3a5192f656993046679f601d5
Andrew Horsman <minirobotics@gmail.com>
2010-09-27 19:50:03 -0400

    Added constructor to GameWindow, started Block class to hold a position, orientation and type of block.

--- docs/planning/draw_blocks.rb
+++ docs/planning/draw_blocks.rb
@@ -10,4 +10,35 @@ require 'block_prototype'
 
 class GameWindow < Gosu::Window
 
+  def initialize
+    super(800, 420, false)
+    
+    self.caption = "Block Drawing Prototype | rgb-tetris"
+  end
+
+end
+
+class Block
+  
+  def initialize(window, block_prototype)
+    @window    = window
+    @cur_x     = 1
+    @cur_y     = 1
+    @cur_r     = :up
+    @box_image = Gosu::Image(@window, "box.png", false) 
+    @block     = case block_prototype
+      when :i then Tetrominoes::MinoI
+      when :j then Tetrominoes::MinoJ
+      when :l then Tetrominoes::MinoL
+      when :o then Tetrominoes::MinoO
+      when :s then Tetrominoes::MinoS
+      when :z then Tetrominoes::MinoZ
+      when :t then Tetrominoes::MinoT
+    end
+  end
+  
+  def draw
+    x, y = @cur_x * 30, @cur_y * 30
+  end
+  
 end
