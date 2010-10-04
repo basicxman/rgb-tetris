3b1e33e3dc08e7be3ebf421474c96cab57127b83
Andrew Horsman <minirobotics@gmail.com>
2010-10-03 22:12:04 -0400

    Fully documented state_machine.rb

--- docs/planning/state_machine.rb
+++ docs/planning/state_machine.rb
@@ -12,8 +12,8 @@ class KeyboardStateMachine
   # Creates a new KeyboardStateMachine instance with a blank actions Hash.
   #
   # @example create a new state machine
-  # keyboard = KeyboardStateMachine.new
-  # keyboard.define_action(....)
+  #   keyboard = KeyboardStateMachine.new
+  #   keyboard.define_action(....)
   def initialize
     @actions = {}
   end
@@ -27,10 +27,10 @@ class KeyboardStateMachine
   # @raise AlreadyDefinedAction if the action already exists in the instance's Hash.
   # @raise NoProcedureGiven if no block is given to execute
   #
-  # @example defining an action which refreshes state after five cycles.
-  # keyboard_state_machine.define_action(:move_left, 5) do
-  #   character.position.x -= 1
-  # end
+  # @example Defining an action which refreshes state after five cycles.
+  #   keyboard_state_machine.define_action(:move_left, 5) do
+  #     character.position.x -= 1
+  #   end
   def define_action(symbol_call, threshold, &proc)
     raise AlreadyDefinedAction, "Action #{symbol_call.inspect} is already defined." if @actions.member? symbol_call
     raise NoProcedureGiven, "No block given to execute." unless block_given?
@@ -38,6 +38,14 @@ class KeyboardStateMachine
     @actions[symbol_call] = { :state => :none, :method => proc, :iterator => 1, :threshold => threshold }
   end
   
+  # Executes an action via it's symbol reference if it's current state is :none
+  #
+  # @param [Symbol] symbol_call the action's reference symbol
+  #
+  # @raise InvalidAction if the action does not exist in the instance's Hash
+  #
+  # @example Calling the move_left action
+  #   keyboard_state_machine.execute_if_clear(:move_left)
   def execute_if_clear(symbol_call)
     raise InvalidAction, "Action does not exist." unless @actions.member? symbol_call
     
@@ -48,6 +56,11 @@ class KeyboardStateMachine
     end
   end
   
+  # Increments the iterator of every action not at the :none state, resets them
+  # to :none when they reach their threshold.
+  #
+  # @example Advancing the state of all actions in an instance
+  #   keyboard_state_machine.advance_state
   def advance_state
     @actions.each_key do |action|
       increment_iterator_of action if check_state_of? action, :just_pressed
@@ -56,26 +69,66 @@ class KeyboardStateMachine
   
   protected
   
+    # Compares the state of an action via it's symbol reference to a given state.
+    #
+    # @param [Symbol] symbol_call the action's symbol reference.
+    # @param [Symbol] state the state to compare to
+    #
+    # @example checking whether the move_left action is at a :none state.
+    #   check_state_of(:move_left, :none)
+    #
+    # @return [Boolean] returns true if the state of the action being checked is
+    # equal to the state given.
     def check_state_of?(symbol_call, state)
       @actions[symbol_call][:state] == state
     end
     
+    # Checks if an action has reached it's iteration threshold.
+    #
+    # @param [Symbol] symbol_call the action's symbol reference
+    #
+    # @example checks whether the move_left action has reached it's threshold.
+    #   reached_threshold?(:move_left)
+    # @return [Boolean] returns true if the given action has reached it's threshold.
     def reached_threshold?(symbol_call)
       @actions[symbol_call][:iterator] == @actions[symbol_call][:threshold]
     end
     
+    # Increments the iterator of an action via it's symbol reference, resets the
+    # iterator and state to :none if the action has reached it's threshold.
+    #
+    # @param [Symbol] symbol_call the action's symbol reference
+    #
+    # @example increment the iterator of the move_left action
+    #   increment_iterator_of(:move_left)
+    # 
+    # @return [Fixnum] returns the new value of the action's iterator.
     def increment_iterator_of(symbol_call)
       @actions[symbol_call][:iterator] += 1
       if reached_threshold? symbol_call
         reset_iterator_of symbol_call
         change_state_of   symbol_call, :none
       end
+      return @actions[symbol_call][:iterator]
     end 
     
+    # Resets the action's iterator to 1.
+    #
+    # @param [Symbol] symbol_call the action's symbol reference
+    #
+    # @example reseting move_left's iterator.
+    #   reset_iterator_of(:move_left)
     def reset_iterator_of(symbol_call)
       @actions[symbol_call][:iterator] = 1
     end
     
+    # Changes the state of an action via it's symbol reference to a new given state.
+    # 
+    # @param [Symbol] symbol_call the action's symbol reference
+    # @param [Symbol] new_state the new state to give the action.
+    # 
+    # @example set the state of move_left to just_pressed
+    #   change_state_of(:move_left, :just_pressed)
     def change_state_of(symbol_call, new_state)
       @actions[symbol_call][:state] = new_state
     end   
